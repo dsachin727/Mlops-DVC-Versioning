2a1eb7d92dcdaf82aa7ab37e84907285cdd14dd1
Sachin Dubey <dsachin727@gmail.com>
2026-09-06 19:25:26 +0530

    Initial commit

---
+++ mycode.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+data = {'Name': ['Sachin', 'Rahul', 'Saurav', 'McArthy'],
+        'Age': [47, 48, 49, 50],
+        'Country': ['India', 'India', 'India', 'Australia']}
+
+df = pd.DataFrame(data)
+
+#print(df)
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+#to make path for data
+filepath = os.path.join(data_dir, 'sample_data.csv')
+
+df.to_csv(filepath, index=False)
+
+print(f"File saved to {filepath}")
